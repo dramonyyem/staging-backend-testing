cad03d1dead2e1d8fa79ccf018d3c4b0d057fb0d
dramonyyem <yem.daramony@gmail.com>
2025-12-18 09:51:08 +0700

    staging update

--- src/middleware/authGuard.js
+++ src/middleware/authGuard.js
@@ -2,10 +2,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const authGuard = (req, res, next) => {
-  const token = req.headers.authorization;
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
-  const decoded = jwt.verify(token, JWT_SECRET);
-  req.user = decoded;
+  // const token = req.headers.authorization;
+  // if (!token) return res.status(401).json({ message: "Unauthorized" });
+  // const decoded = jwt.verify(token, JWT_SECRET);
+  // req.user = decoded;
 
   // console.log(req.user);
   next();
